a6dc116ad15cb86bf9c1f6dcb47b77f6e0596b3f
Piyush3131 <piyushjiskar@gmail.com>
2021-07-06 11:16:35 +0530

    Minor change

--- Reddit.py
+++ Reddit.py
@@ -6,7 +6,7 @@ reddit = praw.Reddit(
     client_id="-----", #Enter your account information here
     client_secret="-----",
     password="-----",
-    user_agent="Reddit post and comment deleter by [github url insert]",
+    user_agent="Reddit post and comment deleter by github.com/Piyush3131",
     username="-----",
 )
 
@@ -22,7 +22,7 @@ def post():                                    #Function for deleting post
     for x in user.submissions.new():
         x.delete()
     print("Posts successfully deleted!")
-    
+
 def main(argv):
     for argument in argv:
         if(argument == argv[0]):
